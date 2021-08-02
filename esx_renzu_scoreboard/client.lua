b45e5bcc4cf0557e7b2eb1d9e85b29045e3ca0ee
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-08-02 19:09:23 +0800

    fix(client): cursor not closing after upload

--- esx_renzu_scoreboard/client.lua
+++ esx_renzu_scoreboard/client.lua
@@ -21,6 +21,8 @@ end)
 
 RegisterNUICallback('avatarupload', function(data, cb)
     TriggerServerEvent('renzu_scoreboard:avatarupload',data.url)
+    SetNuiFocus(false,false)
+    SetNuiFocusKeepInput(false)
 end)
 
 function OpenScoreboard()
